a1270f26352e7cb0b12bb600f34769011743fdc6
jcarley <jcarley@gettyimages.com>
2014-11-16 12:34:11 -0600

    Grants database user rights to create a database.

--- scripts/database.sh
+++ scripts/database.sh
@@ -82,6 +82,7 @@ service postgresql restart
 cat << EOF | su - postgres -c psql
 -- Create the database user:
 CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS';
+ALTER ROLE $APP_DB_USER WITH CREATEDB;
 
 -- Create the database:
 CREATE DATABASE $APP_DB_NAME WITH OWNER $APP_DB_USER;
