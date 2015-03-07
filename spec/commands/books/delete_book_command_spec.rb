d0e490d6d859c21a027e7b32eec4b22a5427efd2
jcarley <jcarley@gettyimages.com>
2015-03-07 07:14:11 -0600

    Adds tests for delete and import.

---
+++ spec/commands/books/delete_book_command_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Books::DeleteBookCommand do
+
+  describe "public methods" do
+    subject { Books::DeleteBookCommand.new(1) }
+    it { is_expected.to respond_to(:id) }
+  end
+
+  describe "#execute" do
+    subject { Books::DeleteBookCommand.new(book.id) }
+
+    let!(:book) { FactoryGirl.create(:book) }
+
+    it "deletes a book" do
+      expect { subject.execute }.to change(Book, :count).by(-1)
+    end
+  end
+
+end
---
+++ spec/commands/books/import_book_command_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe Books::ImportBookCommand do
+end
